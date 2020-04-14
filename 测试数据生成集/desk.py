d9218546488961c98713cd73c428d51ae427cf08
JasmineChen123 <1399426055@qq.com>
2020-04-14 09:44:52 +0800

    'desk模型对应的生成方法'

--- 测试数据生成集/desk.py
+++ 测试数据生成集/desk.py
@@ -1,7 +1,8 @@
 
 
 import os
-import MySQLdb
+# import MySQLdb
+import pymysql
 from db import user1
 import hashlib
 import time
@@ -9,30 +10,45 @@ import time
 cur = user1.db.cursor()
 
 
-for num in range(1, 10):
-    desk_number = str(num)
-    floor = str(1)
-    plug_state = 'false'
-    sql = "insert into login_desk(desk_number,floor,plug_state) values ('" + \
-        desk_number+"','"+floor+"',"+plug_state+")"
-    cur.execute(sql)
+for num in range(1, 26):
+    desk_id = num
+    floor = 1
+    plug_state = 0
+    rent_state =0
+
+    cur.execute("insert into Desk(desk,floor,plug,status) values (%s,%s,%s,%s)",
+                ([desk_id,floor,plug_state,rent_state]))
     user1.db.commit()
 
-for num in range(11, 20):
-    desk_number = str(num)
-    floor = str(2)
-    plug_state = 'false'
-    sql = "insert into login_desk(desk_number,floor,plug_state) values ('" + \
-        desk_number+"','"+floor+"',"+plug_state+")"
-    cur.execute(sql)
+
+for num in range(26, 51):
+    desk_id = num
+    floor = 2
+    plug_state = 0
+    rent_state =0
+
+    cur.execute("insert into Desk(desk,floor,plug,status) values (%s,%s,%s,%s)",
+                ([desk_id,floor,plug_state,rent_state]))
     user1.db.commit()
 
-for num in range(21, 30):
-    desk_number = str(num)
-    floor = str(3)
-    plug_state = 'false'
-    sql = "insert into login_desk(desk_number,floor,plug_state) values ('" + \
-        desk_number+"','"+floor+"',"+plug_state+")"
-    cur.execute(sql)
+for num in range(51, 76):
+    desk_id = num
+    floor = 3
+    plug_state = 0
+    rent_state =0
+
+    cur.execute("insert into Desk(desk,floor,plug,status) values (%s,%s,%s,%s)",
+                ([desk_id,floor,plug_state,rent_state]))
     user1.db.commit()
+
+for num in range(76, 101):
+    desk_id = num
+    floor = 4
+    plug_state = 0
+    rent_state =0
+
+    cur.execute("insert into Desk(desk,floor,plug,status) values (%s,%s,%s,%s)",
+                ([desk_id,floor,plug_state,rent_state]))
+    user1.db.commit()
+
 user1.db.close()
