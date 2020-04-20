aae9c62d095df5149c86fec332a7a260284768b3
awsl1784597340 <grandgadsix@sjtu.edu.cn>
2020-04-20 12:13:57 +0800

    测试数据集中的sql语句改动

--- 测试数据生成集/insert.sql
+++ 测试数据生成集/insert.sql
@@ -1,2 +1,2 @@
 SELECT * FROM user.rent;
-insert into user.rent(date,start_time,end_time,operate_time,status,desk_number_id,user_number_id) values('2020-04-10',11,12,'2020-04-10 08:46:21.533721',1,1,1);
+insert into user.rent(date,start_time,end_time,operate_time,status,desk_number_id_id,user_number_id_id) values('2020-04-10',11,12,'2020-04-10 08:46:21.533721',1,1,1);
