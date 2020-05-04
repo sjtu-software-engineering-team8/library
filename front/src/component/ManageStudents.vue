1157b1f65f1c60a7bdcc7a4e7663fb0873aba7ad
RidiculousDoge <313865185@qq.com>
2020-05-04 14:36:38 +0800

    实现按颜色显示查询结果

--- front/src/component/ManageStudents.vue
+++ front/src/component/ManageStudents.vue
@@ -2,154 +2,114 @@
 <template>
   <div class="query">
     <div v-if="this.$store.state.cookie">
-      <el-container style=" width: 850px; border: 1px solid #eee">
+      <el-container style="height: 1000px; width: 850px; border: 1px solid #eee">
         <el-header style="text-align: center; font-size: 12px;">
           <h1 class="mainTitle">查询页面</h1>
         </el-header>
+        <div>
+          <el-button
+            type="primary"
+            @click.native="query"
+            icon="el-icon-search"
+            style="float:right"
+          >查询</el-button>
+        </div>
         <el-main>
-          <!--<el-button type="primary" size="mini" icon="el-icon-search" @click.native= "query">查询</el-button>-->
           <el-container style="height: 150px; border: 1px solid #eee">
             <el-header style="text-align: center; font-size: 12px">
               <h3>预约结果</h3>
             </el-header>
             <div>
-              <el-table :data="rent_record" border stripe style="width: 100%">
-                <el-table-column label="座位号" prop="desk_number_id_id" width="100"></el-table-column>
+              <el-table
+                style="width: 493%; height: 72px; font-size: 13px; line-height: 0px;"
+                :data="rent_record"
+                border
+                stripe
+                fit="false"
+                show-header="false"
+              >
+                <el-table-column label="座位号" prop="desk_number" width="100"></el-table-column>
                 <el-table-column label="楼层号" prop="floor" width="180"></el-table-column>
                 <el-table-column label="插座情况" prop="plug_state"></el-table-column>
+                <el-table-column label="开始时间" prop="start_time"></el-table-column>
+                <el-table-column label="结束时间" prop="end_time"></el-table-column>
+                <el-table-column label="预约日期" prop="date"></el-table-column>
                 <el-table-column align="right"></el-table-column>
               </el-table>
             </div>
           </el-container>
-          <el-row>
-            <el-col :span="24">
-              <div class="grid-content bg-purple-dark"></div>
-            </el-col>
-          </el-row>
-          <!--
-          <div>
-            <el-container style="height: 500px; border: 1px solid #eee">
-              <el-header style="text-align: center; font-size: 12px">
-                <h3>图书馆空座情况</h3>
-              </el-header>
-              <el-form :inline="true">
-                <el-form-item style="float:left">
-                  <el-input v-model="searchInfo" placeholder="搜索需要的内容" prefix-icon="el-icon-search"></el-input>
-                </el-form-item>
-                <el-form-item>
-                  <el-button
-                    type="primary"
-                    size="mini"
-                    icon="el-icon-search"
-                    @click.native="search()"
-                  >搜索</el-button>
-                </el-form-item>
-              </el-form>
-              <el-row>
-                <el-col :span="24">
-                  <div class="grid-content bg-purple-dark"></div>
-                </el-col>
-              </el-row>
-              <el-table :data="message" border stripe style="width: 100%">
-                <el-table-column label="座位号" prop="desk_number" width="100"></el-table-column>
-                <el-table-column label="楼层号" prop="floor" width="180"></el-table-column>
-                <el-table-column label="插座情况" prop="plug_state"></el-table-column>
-                <el-table-column align="right"></el-table-column>
-              </el-table>
-            </el-container>
-          </div>
-          -->
-
-          <el-container style="height: 120px; border: 1px solid #eee">
-            <el-header style="text-align: center; font-size: 12px">
+          <div style="height: 174px; border: 1px solid rgb(238, 238, 238);">
+            <div style="text-align: center; font-size: 12px;top-padding:10px;">
               <h3>图书馆空座情况</h3>
-            </el-header>
-            <el-main>
-              <el-form :inline="true">
-                <el-form-item style="float:left">
-                  <el-input v-model="searchInfo" placeholder="搜索需要的内容" prefix-icon="el-icon-search"></el-input>
-                </el-form-item>
-                <el-form-item>
-                  <el-button
-                    type="primary"
-                    size="mini"
-                    icon="el-icon-search"
-                    @click.native="search()"
-                  >搜索</el-button>
-                </el-form-item>
-              </el-form>
-            </el-main>
-          </el-container>
-          <el-container>
-            <el-aside style="float:left; width:180px">
-              <el-container style="border: 1px solid #eee">
-                <el-header>
-                  <h5>第一层</h5>
-                </el-header>
-                <div v-for="rowNum in row">
-                  <img :src="imageUrl" style="width:10%" />
-                  <img :src="imageUrl" style="width:10%" />
-                  <img :src="imageUrl" style="width:10%" />
-                  <img :src="imageUrl" style="width:10%" />
-                  <img :src="imageUrl" style="width:10%" />
+            </div>
+            <el-col style="height: 125px; width: 49.8914%;" :xs="12" :sm="12">
+              <div class="grid-content" v-for="rowNum in row">
+                {{rowNum}}.
+                <!--对应desk_id:rowNum*5-4-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor1_row1(rowNum)"></i>
+                <!--对应desk_id:rowNum*5-3-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor1_row2(rowNum)"></i>
+                <!--对应desk_id:rowNum*5-2-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor1_row3(rowNum)"></i>
+                <!--对应desk_id:rowNum*5-1-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor1_row4(rowNum)"></i>
+                <!--对应desk_id:rowNum*5-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor1_row5(rowNum)"></i>
+              </div>
+              <p style="text-align:center;">第一层</p>
+            </el-col>
+            <el-col style="height: 126px;" :xs="12" :sm="12">
+              <div class="grid-content" v-for="rowNum in row">
+                {{rowNum}}.
+                <!--对应desk_id:rowNum*5+21-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor2_row1(rowNum)"></i>
+                <!--对应desk_id:rowNum*5+22-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor2_row2(rowNum)"></i>
+                <!--对应desk_id:rowNum*5+23-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor2_row3(rowNum)"></i>
+                <!--对应desk_id:rowNum*5+24-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor2_row4(rowNum)"></i>
+                <!--对应desk_id:rowNum*5+25-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor2_row5(rowNum)"></i>
+              </div>
+              <p style="text-align:center;">第二层</p>
+            </el-col>
+            <el-row style="height: 366px;">
+              <el-col style="height: 144px; padding-top: 0px; margin-top: 80px;" :xs="12" :sm="12">
+                <div class="grid-content" v-for="rowNum in row">
+                  {{rowNum}}.
+                  <!--对应desk_id:rowNum*5+46-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor3_row1(rowNum)"></i>
+                <!--对应desk_id:rowNum*5+47-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor3_row2(rowNum)"></i>
+                <!--对应desk_id:rowNum*5+48-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor3_row3(rowNum)"></i>
+                <!--对应desk_id:rowNum*5+49-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor3_row4(rowNum)"></i>
+                <!--对应desk_id:rowNum*5+50-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor3_row5(rowNum)"></i>
                 </div>
-              </el-container>
-            </el-aside>
-            <el-main style="width:800px">
-              <!--剩下三个区域-->
-              <el-container style="border: 1px solid #eee">
-                <el-aside style="float:left;width:180px">
-                  <el-container style="border: 1px solid #eee">
-                    <el-header>
-                      <h5>第二层</h5>
-                    </el-header>
-                    <div v-for="rowNum in row">
-                      <input type="checkbox" />
-                      <input type="checkbox" />
-                      <input type="checkbox" />
-                      <input type="checkbox" />
-                      <input type="checkbox" />
-                    </div>
-                  </el-container>
-                </el-aside>
-                <el-main>
-                  <!--剩下两个区域-->
-                  <el-container style="border: 1px solid #eee">
-                    <el-aside style="float:left; width:180px">
-                      <el-container>
-                        <el-header>
-                          <h5>第三层</h5>
-                        </el-header>
-                        <div v-for="rowNum in row">
-                          <input type="checkbox" />
-                          <input type="checkbox" />
-                          <input type="checkbox" />
-                          <input type="checkbox" />
-                          <input type="checkbox" />
-                        </div>
-                      </el-container>
-                    </el-aside>
-                    <el-main>
-                      <el-container style="border: 1px solid #eee">
-                        <el-header>
-                          <h5>第四层</h5>
-                        </el-header>
-                        <el-aside style="float:left; width:180px">
-                          <div v-for="rowNum in row">
-                            <input type="checkbox" />
-                            <input type="checkbox" />
-                            <input type="checkbox" />
-                            <input type="checkbox" />
-                            <input type="checkbox" />
-                          </div>
-                        </el-aside>
-                      </el-container>
-                    </el-main>
-                  </el-container>
-                </el-main>
-              </el-container>
-            </el-main>
-          </el-container>
+                <p style="text-align:center;">第三层</p>
+              </el-col>
+              <el-col style="height: 143px; margin-top: 80px; margin-left: 0px;" :xs="12" :sm="12">
+                <div class="grid-content" v-for="rowNum in row">
+                  {{rowNum}}.
+                  <!--对应desk_id:rowNum*5+71-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor4_row1(rowNum)"></i>
+                <!--对应desk_id:rowNum*5+72-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor4_row2(rowNum)"></i>
+                <!--对应desk_id:rowNum*5+73-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor4_row3(rowNum)"></i>
+                <!--对应desk_id:rowNum*5+74-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor4_row4(rowNum)"></i>
+                <!--对应desk_id:rowNum*5+75-->
+                <i class="el-icon-reading" style="width:20px;" :id="generateId_floor4_row5(rowNum)"></i>
+                </div>
+                <p style="text-align:center;">第四层</p>
+              </el-col>
+            </el-row>
+            <div></div>
+          </div>
         </el-main>
         <el-footer></el-footer>
       </el-container>
@@ -169,46 +129,131 @@ export default {
   name: "manageStudents",
   data() {
     return {
+      flag: false, // 是否查询成功
       imageUrl: imageU,
-      message: [
-        { desk_number: "1", floor: "1", plug_state: "0" },
-        { desk_number: "2", floor: "3", plug_state: "1" }
-      ], // 空座信息，数组形式
-      rent_record: [{ desk_number_id_id: "1", floor: "3", plug_state: "0" }], //账户预约记录
-      searchInfo: "",
-      row: [1, 2, 3, 4, 5],
-      col: [1, 2, 3, 4, 5],
-      floor: [1, 2, 3, 4]
+      message: [], // 全部座位信息，数组形式
+      rent_record: [], //账户预约记录
+      colorStatus:[],   //保存对应每个座位的颜色信息，如：[{desk_id:1,color:'red'},{desk_id:2,color:'green'},...]
+      row: [1, 2, 3, 4, 5]
     };
   },
   methods: {
     query() {
-      alert("testQuery");
       this.$http
         .get("seats/search?" + "No=" + this.$store.state.No)
         .then(result => {
-          if (result.body.status === 0) {
+          if (result.body.status === 0 || result.body.status === 1) {
             //查询成功
+            this.flag= true;
+            console.log(result.body.rent);
             this.message = result.body.message;
             this.rent_record = result.body.rent;
-            console.log(this.message);
             console.log(this.rent_record);
-          } else alert("查询失败！");
+            this.getColorStatus();
+            this.changeColor();
+          } 
+          else {
+            this.flag=false;
+            alert("查询失败！");
+          }
         });
     },
-    created() {
-      this.query();
-      alert("testCreate");
+    getColorStatus(){
+      if(!this.flag){
+          return;
+        }
+
+        for(var i=0;i<this.message.length;i++){
+          if(this.message[i].rent_state==1){// 若unavailable则标红
+            var tmp={"desk_id":this.message[i].desk_id,"color":'red'};
+            this.colorStatus.push(tmp);
+          }
+          else{
+            if(this.message[i].plug_state==0){
+              //若有插座则标绿
+              var tmp={'desk_id':this.message[i].desk_id,'color':'green'};
+              this.colorStatus.push(tmp);
+            }
+            else{
+              // 若无插座则标白
+              var tmp={'desk_id':this.message[i].desk_id,'color':'yellow'};
+              this.colorStatus.push(tmp);
+            }
+          }
+        }
+        for(var i=0;i<this.colorStatus.length;i++){
+        console.log(this.colorStatus[i]);
+        }
     },
-    search() {
-      alert("testSearch");
-      console.log(this.message);
+    changeColor(){
+      alert('before change!');
+      // 第i个id对应colorStatus里第i个对象
+      for(var i=1;i<this.colorStatus.length;i++){
+      document.getElementById(i).style.color=this.colorStatus[i].color;
+      }
+      alert('changed!');
+    },
+    generateId_floor1_row1(index){
+      return index*5-4;
+    },
+    generateId_floor1_row2(index){
+      return index*5-3;
+    },
+    generateId_floor1_row3(index){
+      return index*5-2;
+    },
+    generateId_floor1_row4(index){
+      return index*5-1;
+    },
+    generateId_floor1_row5(index){
+      return index*5;
+    },
+    generateId_floor2_row1(index){
+      return index*5+21;
+    },
+    generateId_floor2_row2(index){
+      return index*5+22;
+    },
+    generateId_floor2_row3(index){
+      return index*5+23;
+    },
+    generateId_floor2_row4(index){
+      return index*5+24;
+    },
+    generateId_floor2_row5(index){
+      return index*5+25;
+    },
+    generateId_floor3_row1(index){
+      return index*5+46;
+    },
+    generateId_floor3_row2(index){
+      return index*5+47;
+    },
+    generateId_floor3_row3(index){
+      return index*5+48;
+    },
+    generateId_floor3_row4(index){
+      return index*5+49;
+    },
+    generateId_floor3_row5(index){
+      return index*5+50;
+    },
+    generateId_floor4_row1(index){
+      return index*5+71;
+    },
+    generateId_floor4_row2(index){
+      return index*5+72;
+    },
+    generateId_floor4_row3(index){
+      return index*5+73;
+    },
+    generateId_floor4_row4(index){
+      return index*5+74;
+    },
+    generateId_floor4_row5(index){
+      return index*5+75;
     }
-  } //,
-  /*
-  created() {
-    this.query();
-  }*/
+    }
 };
 </script>
 
@@ -245,6 +290,7 @@ export default {
   font-family: "楷体";
 }
 
+
 .el-row {
   margin-bottom: 20px;
   &:last-child {
@@ -264,8 +310,9 @@ export default {
   background: #e5e9f2;
 }
 .grid-content {
-  border-radius: 4px;
-  min-height: 36px;
+  border-radius: 10px;
+  min-height: 0px;
+  height: 0px;
 }
 .row-bg {
   padding: 10px 0;
